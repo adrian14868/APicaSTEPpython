f25659a6c2d6ac761eff9c0c3ce972d557a14e55
adrian14868 <55328210+adrian14868@users.noreply.github.com>
2019-09-14 14:57:58 -0400

    Lesson 1
    Adventure

---
+++ lesson1/text_adventure.py
@@ -0,0 +1,22 @@
+print("Hello!")
+
+answer = input("Do you want to go on an adventure (type yes or no)? ")
+if answer == "yes":
+  print("Once upon a time...")
+  #continue your story here using print and input statements
+  Town = input("Where did your journey begin? ")
+  print(Town + " is a long way from here.")
+  Wizard = input("Who brought you here? (type Yoda or Gandolf)")
+  print(Wizard + ", I haven't seen him in years.")
+  
+
+
+
+
+
+
+
+else:
+  print("Ok")
+  
+print("The end")
