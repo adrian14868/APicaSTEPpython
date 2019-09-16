1dab931a108c0ee7cf96d70ddd75e9db0c1ff94f
adrian14868 <55328210+adrian14868@users.noreply.github.com>
2019-09-16 18:46:52 -0400

    Update text_adventure.py

--- lesson1/text_adventure.py
+++ lesson1/text_adventure.py
@@ -4,12 +4,27 @@ answer = input("Do you want to go on an adventure (type yes or no)? ")
 if answer == "yes":
   print("Once upon a time...")
   #continue your story here using print and input statements
-  Town = input("Where did your journey begin? ")
+  Town = input("--Where did your journey begin? ")
   print(Town + " is a long way from here.")
-  Wizard = input("Who brought you here? (type Yoda or Gandolf)")
-  print(Wizard + ", I haven't seen him in years.")
-  
-
+  answer = input("--Who brought you here? (type Yoda or Gandolf)")
+  if answer == "Yoda": 
+    print("--Yoda? I haven't heard from him in years...")
+    print(" ")
+    print(" ")
+    print("--Since he died years ago you lier")
+    print(" ")
+    print(" ")
+    print("*-He zaps you with his wand and you die*")
+ 
+ 
+ 
+ 
+ 
+  else: 
+   print("--That traitor thinks he can send me a spy!")
+   print(" ")
+   print(" ")
+   print("*-He zaps you with his wand and you die*")
 
 
 
